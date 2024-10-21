7a95cf07d3ca57300a530c007e0e71949e7efe65
Andy Tawse <andy.tawse+work@gmail.com>
2024-10-21 21:55:54 +0100

    Add comment

--- src/CropsByMonth.php
+++ src/CropsByMonth.php
@@ -6,6 +6,11 @@ use Symfony\Component\Clock\ClockInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Crop;
 
+/**
+ * A class to try out services in Symphony
+ *
+ * Maybe this could be combined with a repository?
+ */
 class CropsByMonth
 {
     public function __construct(
@@ -14,6 +19,12 @@ class CropsByMonth
     ) {
     }
 
+    /**
+     * Return crops that can be planted in the current month.
+     *
+     * @return array
+     *   An array of Crop objects,
+     */ 
     public function getCropsToPlantInCurrentMonth(): array
     {
         $crops = $this->entityManager->getRepository(Crop::class)->findBy(
